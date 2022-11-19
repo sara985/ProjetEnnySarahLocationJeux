c968b5e647cdb52a7fbce21f347f7985667f189b
Sarah <saraharcenciel@gmail.com>
2022-11-19 21:34:36 +0100

    added first validation rule for first name field

---
+++ ProjetEnnySarahLocationJeux/ValidationRules/TextValidationRule.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace ProjetEnnySarahLocationJeux.ValidationRules
+{
+    public class TextValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(Regex.IsMatch(value.ToString(), "^[A-Za-z]*$"))
+            {
+                return ValidationResult.ValidResult;
+            }
+            if(value.ToString().Length==0)
+            {
+                return new ValidationResult(false, "empty");
+            }
+            return new ValidationResult(false, "invalid");
+        }
+    }
+}
