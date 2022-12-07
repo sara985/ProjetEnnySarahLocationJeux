a9c6cedc73c668af5face9a0c4f8009a117f22d4
EnnyMJE <78366762+EnnyMJE@users.noreply.github.com>
2022-12-07 09:42:25 +0100

    admin part

---
+++ ProjetEnnySarahLocationJeux/DAO/AdminDao.cs
@@ -0,0 +1,33 @@
+﻿using ProjetEnnySarahLocationJeux.Interfaces;
+using ProjetEnnySarahLocationJeux.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetEnnySarahLocationJeux.DAO
+{
+    internal class AdminDao : IDAOInterface<Administrator>
+    {
+        public Administrator GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(Administrator t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Administrator> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Administrator t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
