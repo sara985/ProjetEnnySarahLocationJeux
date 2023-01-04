23fbdafd957585f6c8e8a3a220693103018bbbad
sss985 <117765598+sss985@users.noreply.github.com>
2023-01-04 18:10:59 +0100

    test

---
+++ ProjetEnnySarahLocationJeux/Viewmodels/MyGamesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ProjetEnnySarahLocationJeux.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetEnnySarahLocationJeux.Viewmodels
+{
+    public class MyGamesViewModel : ViewModelBase
+    {
+        List<VideoGame> _rentedGames;
+        List<VideoGame> _bookedGames;
+        List<VideoGame> _ownedGames;
+
+        public MyGamesViewModel()
+        {
+            RentedGames = new List<VideoGame>();
+            _bookedGames = new List<VideoGame>();
+            RentedGames.Add(new VideoGame(1,200,"wii",5,"wii",new List<Copy>()));
+        }
+
+        public List<VideoGame> RentedGames { get => _rentedGames;
+            set
+            {
+                _rentedGames = value;
+                OnPropertyChanged("RentedGames");
+            }
+        }
+    }
+}
